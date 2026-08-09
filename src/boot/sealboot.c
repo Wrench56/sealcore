4ef8ffae18cb5447f1e90261674a9fc2e2e9c5df
Mark Devenyi <markdevenyidev@gmail.com>
2026-08-08 21:44:07 -0400

    feat(boot): load and execute next-stage sealcore binary

--- src/boot/sealboot.c
+++ src/boot/sealboot.c
@@ -3,6 +3,16 @@
 
 #include "gfx/img.h"
 
+#define SEALCORE_SIZE 4 * 1024 * 1024
+
+typedef EFI_STATUS EFIAPI (*sealcore_entry_fn_t)(
+    EFI_HANDLE ImageHandle,
+    EFI_SYSTEM_TABLE* SystemTable
+);
+
+static const EFI_GUID sfs_guid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+static EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* sfs_prot = NULL;
+
 static const uint8_t imgdata[] = {
     #include "boot/logo.hex"
 };
@@ -24,12 +34,10 @@ EFI_STATUS EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
     (void) ImageHandle;
 
+    EFI_STATUS status;
+
     SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
     SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World!\r\n");
-    SystemTable->ConOut->OutputString(
-        SystemTable->ConOut,
-        L"Press Enter to continue...\r\n"
-    );
 
     setup_gfx(SystemTable);
     image_t image;
@@ -38,6 +46,60 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
     image.data = (pixel_data_t*) &imgdata;
     draw_img(&image, 0, 0);
 
+    status = SystemTable->BootServices
+        ->LocateProtocol((EFI_GUID*) &sfs_guid, NULL, (VOID**) &sfs_prot);
+    if (EFI_ERROR(status)) {
+        return status;
+    }
+
+    EFI_FILE_PROTOCOL* bootvol;
+    status = sfs_prot->OpenVolume(sfs_prot, &bootvol);
+    if (EFI_ERROR(status)) {
+        return status;
+    }
+
+    EFI_FILE_PROTOCOL* sealcore_file;
+    status = bootvol->Open(
+        bootvol,
+        &sealcore_file,
+        L"\\EFI\\SEALCORE\\sealcore.bin",
+        EFI_FILE_READ_ONLY,
+        0
+    );
+    if (EFI_ERROR(status)) {
+        return status;
+    }
+
+    EFI_PHYSICAL_ADDRESS sc_phys = 0;
+    UINTN sc_pages = EFI_SIZE_TO_PAGES(SEALCORE_SIZE);
+    status = SystemTable->BootServices->AllocatePages(
+        AllocateAnyPages,
+        EfiLoaderCode,
+        sc_pages,
+        &sc_phys
+    );
+    if (EFI_ERROR(status)) {
+        return EFI_OUT_OF_RESOURCES;
+    }
+
+    uint64_t sc_sz = SEALCORE_SIZE;
+
+    status = sealcore_file->Read(sealcore_file, &sc_sz, (void*) sc_phys);
+    if (EFI_ERROR(status)) {
+        return status;
+    } else if (sc_sz == 0) {
+        return EFI_NOT_FOUND;
+    }
+
+    sealcore_entry_fn_t fn = (sealcore_entry_fn_t) sc_phys;
+    fn(ImageHandle, SystemTable);
+
+    SystemTable->ConOut->OutputString(
+        SystemTable->ConOut,
+        L"Press Enter to continue...\r\n"
+    );
     wait_for_enter(SystemTable);
+
+    SystemTable->BootServices->FreePages(sc_phys, sc_pages);
     return EFI_SUCCESS;
 }
